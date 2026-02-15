d88c9bbf48dd10dcd839ae09a0f237f0b59b91e3
skyer <sky13net84@gmail.com>
2026-02-15 19:36:14 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Задача #1");
+        int firstFriday=6;
+        for (; firstFriday <= 31; firstFriday+=7) {
+            System.out.println("Сегодня пятница," +firstFriday+"-е число. Необходимо подготовить отчет");
+        }
     }
 }
