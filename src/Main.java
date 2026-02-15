3d442bf943103cc04597758a5f7c965c262f943b
skyer <sky13net84@gmail.com>
2026-02-15 20:49:30 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача #1");
-        int firstFriday=6;
-        for (; firstFriday <= 31; firstFriday+=7) {
-            System.out.println("Сегодня пятница," +firstFriday+"-е число. Необходимо подготовить отчет");
+        System.out.println("\nЗадача #1");
+        int firstFriday = 6;
+        for (; firstFriday <= 31; firstFriday += 7) {
+            System.out.println("Сегодня пятница," + firstFriday + "-е число. Необходимо подготовить отчет");
+        }
+        System.out.println("\nЗадача #2 (вариант 1)");
+        int distance = 42195;
+        int start = 0;
+        do {
+            start++;
+            System.out.println(start + " - Держитесь! Осталось " + distance + " метров");
+            distance = distance - 500;
+        } while (distance > 0);
+        System.out.println("\nЗадача #2 (вариант 2)");
+        int start2 = 0;
+        for (int i = 42195; i > 0; ) {
+            start2++;
+            System.out.println(start2 + "_ Держитесь! Осталось " + i + " метров");
+            i = i - 500;
         }
     }
 }
